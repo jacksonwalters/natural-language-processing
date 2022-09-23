a0a1f3a442cb5e92283a557cc8b151a45d35135e
Jackson Walters <jacksonwalters@gmail.com>
2022-09-23 13:16:09 -0400

    add directory for each project

---
+++ text_summarization/text_summarization.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+import re
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+df = pd.read_csv("tennis.csv")
+df.head()
+
+#tokenize words
+from nltk.tokenize import sent_tokenize
+sentences = []
+for s in df['article_text']:
+    sentences.append(sent_tokenize(s))
+
+sentences = [y for x in sentences for y in x]
+
+#Glove word embeddings
+word_embeddings = {}
+f = open('../glove/glove.6B.100d.txt', encoding='utf-8')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    word_embeddings[word] = coefs
+f.close()
+
+#clean sentences
+clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Z]", " ")
+clean_sentences = [s.lower() for s in clean_sentences]
+stop_words = stopwords.words('english')
+def remove_stopwords(sen):
+    sen_new = " ".join([i for i in sen if i not in stop_words])
+    return sen_new
+clean_sentences = [remove_stopwords(r.split()) for r in clean_sentences]
+
+#create vectors for sentences using wordvectors
+sentence_vectors = []
+for i in clean_sentences:
+  if len(i) != 0:
+    v = sum([word_embeddings.get(w, np.zeros((100,))) for w in i.split()])/(len(i.split())+0.001)
+  else:
+    v = np.zeros((100,))
+  sentence_vectors.append(v)
+
+  #use cosine similarity to measure distance between sentences
+sim_mat = np.zeros([len(sentences), len(sentences)])
+from sklearn.metrics.pairwise import cosine_similarity
+for i in range(len(sentences)):
+  for j in range(len(sentences)):
+    if i != j:
+      sim_mat[i][j] = cosine_similarity(sentence_vectors[i].reshape(1,100), sentence_vectors[j].reshape(1,100))[0,0]
+
+import networkx as nx
+
+nx_graph = nx.from_numpy_array(sim_mat)
+scores = nx.pagerank(nx_graph)
+
+ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
+for i in range(5):
+  print("ARTICLE:")
+  print(df['article_text'][i])
+  print('\n')
+  print("SUMMARY:")
+  print(ranked_sentences[i][1])
+  print('\n')
