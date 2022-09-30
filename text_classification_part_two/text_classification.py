0f5987b92cf85d0db102fe6be0c82285441e8bfc
Jackson Walters <jacksonwalters@gmail.com>
2022-09-30 14:24:29 -0400

    create text_classification.py

---
+++ text_classification_part_two/text_classification.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+import tensorflow as tf
+
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+#print some data relevant to tensorflow
+print("Version: ", tf.__version__)
+print("Eager mode: ", tf.executing_eagerly())
+print("Hub version: ", hub.__version__)
+print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
+
+# Split the training set into 60% and 40%, so we'll end up with 15,000 examples
+# for training, 10,000 examples for validation and 25,000 examples for testing.
+train_data, validation_data, test_data = tfds.load(
+    name="imdb_reviews",
+    split=('train[:60%]', 'train[60%:]', 'test'),
+    as_supervised=True)
+
+train_examples_batch, train_labels_batch = next(iter(train_data.batch(10)))
+
+#in a very high-level fashion, build a keras model with a 20dim word embedding
+#and a trainable "hub" layer
+embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
+hub_layer = hub.KerasLayer(embedding, input_shape=[],
+                           dtype=tf.string, trainable=True)
+hub_layer(train_examples_batch[:3])
+#build the model sequentially.
+model = tf.keras.Sequential()
+model.add(hub_layer)
+model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(1))
+
+print(model.summary())
+
+#compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+#train the model for 20 epochs
+history = model.fit(train_data.shuffle(10000).batch(512),
+                    epochs=20,
+                    validation_data=validation_data.batch(512),
+                    verbose=1)
+
+#evaluate the accuracy of the model in its ability to accurately classify
+#movie reviews as positive or negative
+results = model.evaluate(test_data.batch(512), verbose=2)
+
+for name, value in zip(model.metrics_names, results):
+  print("%s: %.3f" % (name, value))
